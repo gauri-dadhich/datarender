f8fba6d01fd9747dfe9b3918e1ceba9a444eff25
gauri-dadhich <gauridadhich16.09@gmail.com>
2024-07-20 09:59:39 +0530

    hey

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// import Apifetch from './Apifetch.jsx'
+import './index.css'
+import App from './App'
+import Assignment from './Assignment'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Assignment/>
+  </React.StrictMode>,
+)
